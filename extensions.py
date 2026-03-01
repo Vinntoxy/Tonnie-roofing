f49b173240676c472f5128b638a1b92848159977
Vinntoxy <119734377+Vinntoxy@users.noreply.github.com>
2026-03-01 16:28:36 +0300

    Update extensions.py

--- extensions.py
+++ extensions.py
@@ -1,5 +1,9 @@
+"""Flask extensions module"""
 from flask_sqlalchemy import SQLAlchemy
+from flask_session import Session
 from flask_migrate import Migrate
 
+# Initialize extensions
 db = SQLAlchemy()
+session = Session()
 migrate = Migrate()
